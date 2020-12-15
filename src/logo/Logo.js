49330a53f07f79dd9e38308323a92035f85f54a1
vinaykangralkar <vinaykangralkar9@gmail.com>
2020-12-15 19:30:46 +1100

    Added nav and logo component

---
+++ src/logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import LogoImage from './logo.png';
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 " options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+                <div className="Tilt-inner pa3"> <img src={LogoImage} /> </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
