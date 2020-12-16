6c15788aeb63b885d6ebc135cc05c5741a263f4f
vinaykangralkar <vinaykangralkar9@gmail.com>
2020-12-16 17:41:04 +1100

    Image link component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
+import { Component } from 'react';
 import Navigation from './components/navigation/Navigation.js';
-import Logo from './logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import Logo from './components/logo/Logo.js';
+import Particles from 'react-particles-js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition /> */}
-    </div>
-  );
+const particleOptions = {
+  
+    particles: {
+      number: {
+        value: 70,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      }
+    }
+}
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonClick = () => {
+    console.log('click');
+  }
+
+  render (){
+    return (
+      <div className="App">
+        <Particles className='particles'
+                params={particleOptions}
+          />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonClick={this.onButtonClick}/>
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -5,7 +5,7 @@ import LogoImage from './logo.png';
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 " options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+            <Tilt className="Tilt br2 " options={{ max : 50 }} style={{ height: 250, width: 250 }} >
                 <div className="Tilt-inner pa3"> <img src={LogoImage} /> </div>
             </Tilt>
         </div>
