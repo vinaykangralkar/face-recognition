2713844be8990a31aca150cf706ac7c8ca087bc7
vinaykangralkar <vinaykangralkar9@gmail.com>
2020-12-20 17:10:59 +1100

    Sign in function

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -12,7 +12,7 @@ const Register = ({onRouteChange}) => {
               <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
             </div>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" fhtmlForor="email-address">Email</label>
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
               <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
             </div>
             <div className="mv3">
