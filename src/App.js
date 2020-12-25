b82408f5c514c9bd3b73b0912f485c632f0af3b6
vinaykangralkar <vinaykangralkar9@gmail.com>
2020-12-25 11:51:41 +1100

    updates

--- src/App.js
+++ src/App.js
@@ -1,56 +1,55 @@
-import { Component } from 'react';
-import Navigation from './components/navigation/Navigation.js';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
-import Rank from './components/Rank/Rank.js';
-import Logo from './components/logo/Logo.js';
-import Signin from './components/Signin/Signin.js';
-import Register from './components/Register/Register';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
-const particleOptions = {
-  
-    particles: {
-      number: {
-        value: 70,
-        density: {
-          enable: true,
-          value_area: 800
-        }
+const particlesOptions = {
+  //customize this to your liking
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
       }
     }
+  }
 }
 
 const initialState = {
-    input: '',
-    imageURL: '',
-    box: {},
-    route: 'signin',
-    isSignedIn: false,
-    user: {
-      id: '',
-      name: '',
-      email: '',
-      entries: 0,
-      joined: ''
-    }
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
 }
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = initialState;
   }
 
-  loadUser = (user) => {
+  loadUser = (data) => {
     this.setState({user: {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      entries: user.entries,
-      joined: user.joined
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
     }})
   }
 
@@ -59,7 +58,6 @@ class App extends Component {
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
@@ -68,77 +66,81 @@ class App extends Component {
     }
   }
 
-  displayBox = (box) => {
-    this.setState({box})
+  displayFaceBox = (box) => {
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value})
+    this.setState({input: event.target.value});
   }
 
-  onButtonClick = () => {
-    this.setState({imageURL: this.state.input});
-    fetch('https://still-mesa-37379.herokuapp.com/imageurl', {
-          method: 'post',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            input: this.state.input
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+      fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
           })
-        })
-    .then(response => response.json())
-    .then(response => {
-      if(response){
-        fetch('https://still-mesa-37379.herokuapp.com/image', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            id: this.state.user.id
-          })
-        })
-        .then(response => response.json())
-        .then(count => {
-          this.setState(Object.assign(this.state.user, {entries: count})) 
-        })
-        .catch(console.log)
-      }
-      this.displayBox(this.calculateFaceLocation(response))
-    })
-    .catch(err => {console.log(err)})
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+            .catch(console.log)
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
-    if(route === 'signout'){
+    if (route === 'signout') {
       this.setState(initialState)
-    }else if(route === 'home'){
+    } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
-    this.setState({route})
+    this.setState({route: route});
   }
 
-  render (){
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <Particles className='particles'
-                params={particleOptions}
+         <Particles className='particles'
+          params={particlesOptions}
         />
-        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
-        {this.state.route === 'home'
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
           ? <div>
               <Logo />
-              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
-              <ImageLinkForm onInputChange={this.onInputChange} onButtonClick={this.onButtonClick}/>
-              <FaceRecognition imageURL={this.state.imageURL} box={this.state.box}/>
+              <Rank
+                name={this.state.user.name}
+                entries={this.state.user.entries}
+              />
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
             </div>
           : (
-            this.state.route === 'signin'?
-            <Signin loadUser={this.loadUser}  onRouteChange={this.onRouteChange}/>
-            :
-            <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-          )
-          
+             route === 'signin'
+             ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            )
         }
-        
-        
       </div>
     );
   }
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Signin extends Component {
-  constructor(props){
+class Signin extends React.Component {
+  constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       signInEmail: '',
       signInPassword: ''
     }
@@ -18,7 +18,7 @@ class Signin extends Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://still-mesa-37379.herokuapp.com/signin', {
+    fetch('http://localhost:3000/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -26,59 +26,59 @@ class Signin extends Component {
         password: this.state.signInPassword
       })
     })
-    .then(response => response.json())
-    .then(user => {
-      if(user.id){
-        this.props.loadUser(user);
-        this.props.onRouteChange('home')
-      }
-    }) 
+      .then(response => response.json())
+      .then(user => {
+        if (user.id) {
+          this.props.loadUser(user)
+          this.props.onRouteChange('home');
+        }
+      })
   }
 
   render() {
-    const {onRouteChange} = this.props;
+    const { onRouteChange } = this.props;
     return (
-      <article className="br3 shadow-5 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
-          <div className="measure ">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0" >
-              <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input 
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                  type="email" 
-                  name="email-address"  
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
                   id="email-address"
-                  onChange={this.onEmailChange}  
+                  onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input 
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                  type="password" 
-                  name="password"  
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
                   id="password"
-                  onChange={this.onPasswordChange}  
+                  onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
             <div className="">
-              <input 
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                type="submit" 
-                value="Sign in"
+              <input
                 onClick={this.onSubmitSignIn}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Sign in"
               />
             </div>
             <div className="lh-copy mt3">
-              <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+              <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             </div>
           </div>
         </main>
       </article>
-    )
+    );
   }
 }
 
